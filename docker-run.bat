ca51d9eb52e98d849b4388c4841cb050a41aa5d1
James Barwick <jbarwick@me.com>
2021-12-03 18:56:55 +0800

    Version Update to: 1.0.5

--- docker-run.bat
+++ docker-run.bat
@@ -12,7 +12,7 @@ call mvn clean package install
 "%GPG%" -ab target\%NAME%-%VERSION%-sources.jar
 "%GPG%" --verify target\%NAME%-%VERSION%.jar.asc
 "%GPG%" --verify target\%NAME%-service-%VERSION%-sources.jar.asc
-# call mvn deploy
+rem call mvn deploy
 call mvn gpg:sign-and-deploy-file \
      -DpomFile=target/%NAME%-%VERSION%.pom \
      -Dfile=target/%NAME%-%VERSION%.jar \
@@ -23,7 +23,7 @@ call mvn gpg:sign-and-deploy-file \
      -Dfile=target/%NAME%-%VERSION%.jar \
      -Durl=http://oss.sonatype.org/service/local/staging/deploy/maven2/ \
      -DrepositoryId=sonatype_oss
-docker build -t %ORGANIZATION%/%NAME%:%VERSION% -m 2GB --build-arg VERSION=%VERSION% .
+rem docker build -t %ORGANIZATION%/%NAME%:%VERSION% -m 2GB --build-arg VERSION=%VERSION% .
 rem I can't afford a subscription
 rem docker scan %ORGANIZATION%/%NAME%:%VERSION%
 rem do NOT put a space after DOCKER_PASSWORD and before the pipe |.  Else login will fail!
